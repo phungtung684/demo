4b55259f098bf3ea75eb77ffc1dcf4fbeb0d68ed
phungvantung <phung.van.tung@framgia.com>
2016-11-24 11:39:56 +0700

    Login with Facebook and Google

--- MoneyLover/MoneyLover/AppDelegate/AppDelegate.swift
+++ MoneyLover/MoneyLover/AppDelegate/AppDelegate.swift
@@ -8,15 +8,34 @@
 
 import UIKit
 import CoreData
+import FBSDKCoreKit
+import GoogleSignIn
+import Google
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     lazy var coreDataManager = CoreDataManager()
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        if let errorConfig = configureError {
+            print(errorConfig)
+        }
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        print("URL: ", url.scheme)
+        if url.scheme == "fb558824467644283" {
+            FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+        } else if url.scheme == "com.googleusercontent.apps.43222908773-mhq0odkbf410hs9ac5aa1e7qeihn6bfe" {
+            return GIDSignIn.sharedInstance().handleURL(url, sourceApplication: sourceApplication, annotation: annotation)
+        }
+        return false
     }
     
     func applicationWillResignActive(application: UIApplication) {
